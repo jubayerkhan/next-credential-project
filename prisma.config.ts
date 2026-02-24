9e6e86499eff0e06273355b61acc71778aaa9189
@jubayer.khan <jubayer.khan@annanovas.com>
2026-02-24 12:38:38 +0600

    prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
-export default {
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-};
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+});
